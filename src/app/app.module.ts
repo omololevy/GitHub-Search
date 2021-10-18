bdceeceea1a717c4809563d57df6d747e60333aa
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-18 06:06:07 +0300

    Add imports of the users and navbar in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
