8d27752e612ca79fd9ad65ab61b99365d4fd3f36
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-19 08:05:44 +0300

    import RouterModule and link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from "./users/users.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'users', pathMatch: 'full'},
+  {path: 'users', component: UsersComponent  },
+  {path: '**',  redirectTo: 'users', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
